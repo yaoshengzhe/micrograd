7f64d47942d10c2959772c019d3830a234527927
Shengzhe Yao <yaoshengzhe@gmail.com>
2024-04-15 00:00:41 -0700

    Add children, label, ops, and a draw method for visualization.

--- examples/value.py
+++ examples/value.py
@@ -1,5 +1,23 @@
 import micrograd
 
 if __name__ == '__main__':
-    a = micrograd.core.Value(2.0)
+    a = micrograd.core.Value(2.0, label='a')
     print(a)
+
+    b = micrograd.core.Value(-3.0, label='b')
+    print(b)
+
+    c = a * b
+    c.label = 'c'
+    print(c)
+    
+
+    d = c + micrograd.core.Value(10, label='e')
+    d.label = 'd'
+    print(d)
+
+    f = micrograd.core.Value(-2.0, label='f')
+    L = d * f
+    L.label = 'L'
+
+    print(L.draw())
--- micrograd/core.py
+++ micrograd/core.py
@@ -1,9 +1,48 @@
 '''Micrograd core abstractions.'''
 
+import graphviz
+
 class Value:
-    def __init__(self, data):
+    def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
+        self._prev = set(_children)
+        self._op = _op
+        self.label = label
 
     def __repr__(self):
         return f"Value(data={self.data})"
 
+    def __add__(self, other):
+        return Value(self.data + other.data, (self, other), '+')
+
+    def __mul__(self, other):
+        return Value(self.data * other.data, (self, other), '*')
+
+    @staticmethod
+    def _trace(root):
+        nodes, edges = set(), set()
+        def build(v):
+            if v not in nodes:
+                nodes.add(v)
+                for child in v._prev:
+                    edges.add((child, v))
+                    build(child)
+        build(root)
+        return nodes, edges
+
+    def draw(self):
+        dot = graphviz.Digraph(format='svg', graph_attr={'rankdir': 'LR'})
+
+        nodes, edges = Value._trace(self)
+        for n in nodes:
+            uid = str(id(n))
+            dot.node(name=uid, label="{ %s | data %.4f }" % (n.label, n.data, ), shape='record')
+
+            if n._op:
+                dot.node(name=uid+n._op, label=n._op)
+                dot.edge(uid+n._op, uid)
+
+        for n1, n2 in edges:
+            dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+
+        return dot
